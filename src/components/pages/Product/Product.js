7eeac816a9a06f138e703f94f755113c61ca26c8
Julia Ramos <juliaramosguedes@gmail.com>
2019-12-14 19:50:32 -0300

    change icon and correct product page

--- src/components/pages/Product/Product.js
+++ src/components/pages/Product/Product.js
@@ -9,12 +9,14 @@ export default function Product({ match, products, addToCart }) {
   let { name, description, quantity, status, image, price, type, size, stock } = product;
   const [qtyAvailable, setQtyAvailable] = useState([]);
   const [selectedQty, setSelectedQty] = useState(quantity);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     for (let i = 1; i <= stock; i += 1) {
       qtyAvailable.push(i);
     }
     setQtyAvailable(qtyAvailable);
+    setLoaded(true);
   }, []);
   
   const maxQty = () => {
@@ -51,42 +53,46 @@ export default function Product({ match, products, addToCart }) {
                   {
                     status ?
                   <>
-                    <label>Quantidade: </label>
-                    <select name="select" onChange={(e) => changeQty(e)}>
-                      <option value={selectedQty} selected>{selectedQty}</option>
-                      <option disabled value> -- </option>
-                      {maxQty()}
-                    </select>
-                    {
-                      size ?
-                      <> 
-                        <br/>
-                        <label>Selecione o tamanho: </label>
-                        <select>
-                          <option value="P">P</option>
-                          <option value="M">M</option>
-                          <option value="G">G</option>
+                    {loaded ? 
+                      <>
+                        <label>Quantidade: </label>
+                        <select name="select" onChange={(e) => changeQty(e)}>
+                          <option value={selectedQty} selected>{selectedQty}</option>
+                          <option disabled value> -- </option>
+                          {maxQty()}
                         </select>
                       </>
+                      : <></>
+                    }
+                    {
+                      size ?
+                        <> 
+                          <br/>
+                          <label>Selecione o tamanho: </label>
+                          <select>
+                            <option value="P">P</option>
+                            <option value="M">M</option>
+                            <option value="G">G</option>
+                          </select>
+                        </>
                         : <></>
                     }
                     {
                       type ?
-                      <> 
-                        <br/>
-                        <label>Escolha o modelo: </label>
-                        <select>
-                          <option value="Feminino">Feminino</option>
-                          <option value="Masculino">Masculino</option>
-                        </select>
-                      </>
+                        <> 
+                          <br/>
+                          <label>Escolha o modelo: </label>
+                          <select>
+                            <option value="Feminino">Feminino</option>
+                            <option value="Masculino">Masculino</option>
+                          </select>
+                        </>
                         : <></>
                     }
                     <Button 
-                      className="sectionC-card-button col" 
-                      variant="dark" 
-                      onClick={() => addProduct(product)}                  
-                    >
+                      className="sectionC-card-button col"
+                      variant="dark"
+                      onClick={() => addProduct(product)}>
                       Adicionar ao carrinho
                     </Button>
                   </>
