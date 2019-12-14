c8d0c90b1d05f766ab075443750be3cad5e1fe8b
Marcus <marcusffbrandao@hotmail.com>
2019-12-14 09:56:50 -0300

    login page updated

--- src/components/pages/User/User.js
+++ src/components/pages/User/User.js
@@ -87,7 +87,9 @@ export default function User({ userID }) {
           {/* <Card style={{ width: '18rem' }}> */}
           <Card.Body>
             {/* <h3 className="user-status">{status}</h3> */}
-            <h3 className="user-title">Suas informações</h3>
+            <div className="user-title-container">
+              <h3 className="user-title">Suas informações</h3>
+            </div>
             <p className="user-instructions">
               Por favor, preencha os campos abaixo para garantir que seus dados
               estejam atualizados.
@@ -103,164 +105,156 @@ export default function User({ userID }) {
                   onChange={e => setName(e.target.value)}
                 />
               </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Endereço de e-mail</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="E-mail"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Foto</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Foto(url)"
-                  value={photoURL}
-                  onChange={e => setPhotoURL(e.target.value)}
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Rua</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Rua"
-                  value={address.street}
-                  onChange={e =>
-                    setAddress({ ...address, street: e.target.value })
-                  }
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Número</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Número"
-                  value={address.number}
-                  onChange={e =>
-                    setAddress({ ...address, number: e.target.value })
-                  }
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Complemento</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Complemento"
-                  value={address.complement}
-                  onChange={e =>
-                    setAddress({ ...address, complement: e.target.value })
-                  }
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Bairro</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Bairro"
-                  value={address.neighborhood}
-                  onChange={e =>
-                    setAddress({ ...address, neighborhood: e.target.value })
-                  }
-                />
-              </Form.Group>
               <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>Cidade</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Cidade"
-                  value={address.city}
-                  onChange={e =>
-                    setAddress({ ...address, city: e.target.value })
-                  }
-                />
-              </Form.Group>
-
-              {/* <Form.Group controlId="formBasicPassword">
-                <Form.Label>Estado</Form.Label>
-                <Form.Control
-                  type="text"
-                  // placeholder="Estado"
-                  value={address.state}
-                  onChange={e =>
-                    setAddress({ ...address, state: e.target.value })
-                  }
-                />
-              </Form.Group> */}
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>Estado</Form.Label>
-                <Form.Control as="select">
-                  value={address.state}
-                  name="state" id="inputState" class="form-control" >
-                  <option>{address.state}</option>
-                  <option>Acre (AC) </option>
-                  <option>Alagoas (AL)</option>
-                  <option>Amapá (AP)</option>
-                  <option>Amazonas (AM)</option>
-                  <option>Bahia (BA)</option>
-                  <option>Ceará (CE)</option>
-                  <option>Distrito Federal (DF)</option>
-                  <option>Espírito Santo (ES)</option>
-                  <option>Goiás (GO)</option>
-                  <option>Maranhão (MA)</option>
-                  <option>Mato Grosso (MT)</option>
-                  <option>Mato Grosso do Sul (MS)</option>
-                  <option>Minas Gerais (MG)</option>
-                  <option>Pará (PA)</option>
-                  <option>Paraíba (PB)</option>
-                  <option>Paraná (PR)</option>
-                  <option>Pernambuco (PE)</option>
-                  <option>Piauí (PI)</option>
-                  <option>Rio de Janeiro (RJ)</option>
-                  <option>Rio Grande do Norte (RN)</option>
-                  <option>Rio Grande do Sul (RS)</option>
-                  <option>Rondônia (RO)</option>
-                  <option>Roraima (RR)</option>
-                  <option>Santa Catarina (SC)</option>
-                  <option>São Paulo (SP)</option>
-                  <option>Sergipe (SE)</option>
-                  <option>Tocantins (TO)</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>CEP</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="FALTOU INCLUIR NO CÓDIGO"
-                  // value={address.cep}
-                  // onChange={e =>
-                  //   setAddress({ ...address, state: e.target.value })
-                  // }
-                />
-              </Form.Group>
+                <Form.Group as={Col} md="8" controlId="formBasicPassword">
+                  <Form.Label>Endereço de e-mail</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="formBasicPassword">
+                  <Form.Label>Telefone</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="FALTOU INCLUIR NO CÓDIGO"
+                    // value=""
+                    // onChange={e =>
+                    //   setAddress({ ...address, state: e.target.value })}
+                  />
+                </Form.Group>
               </Form.Row>
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Telefone</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="FALTOU INCLUIR NO CÓDIGO"
-                  // value=""
-                  // onChange={e =>
-                  //   setAddress({ ...address, state: e.target.value })}
-                />
-              </Form.Group>
+              <Form.Row>
+                <Form.Group as={Col} md="10" controlId="formBasicPassword">
+                  <Form.Label>Rua</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="Rua"
+                    value={address.street}
+                    onChange={e =>
+                      setAddress({ ...address, street: e.target.value })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group as={Col} md="2" controlId="formBasicPassword">
+                  <Form.Label>Número</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="Número"
+                    value={address.number}
+                    onChange={e =>
+                      setAddress({ ...address, number: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md="6" controlId="formBasicPassword">
+                  <Form.Label>Complemento</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="Complemento"
+                    value={address.complement}
+                    onChange={e =>
+                      setAddress({ ...address, complement: e.target.value })
+                    }
+                  />
+                </Form.Group>
 
-              <Button type="submit" className="user-buttons shadow-sm" variant="dark">
+                <Form.Group as={Col} md="6" controlId="formBasicPassword">
+                  <Form.Label>Bairro</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="Bairro"
+                    value={address.neighborhood}
+                    onChange={e =>
+                      setAddress({ ...address, neighborhood: e.target.value })
+                    }
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md="7" controlId="formGridCity">
+                  <Form.Label>Cidade</Form.Label>
+                  <Form.Control
+                    type="text"
+                    // placeholder="Cidade"
+                    value={address.city}
+                    onChange={e =>
+                      setAddress({ ...address, city: e.target.value })
+                    }
+                  />
+                </Form.Group>
+
+                <Form.Group as={Col} md="5" controlId="formGridState">
+                  <Form.Label>Estado</Form.Label>
+                  <Form.Control as="select">
+                    value={address.state}
+                    name="state" id="inputState" class="form-control" >
+                    <option>{address.state}</option>
+                    <option>Acre (AC) </option>
+                    <option>Alagoas (AL)</option>
+                    <option>Amapá (AP)</option>
+                    <option>Amazonas (AM)</option>
+                    <option>Bahia (BA)</option>
+                    <option>Ceará (CE)</option>
+                    <option>Distrito Federal (DF)</option>
+                    <option>Espírito Santo (ES)</option>
+                    <option>Goiás (GO)</option>
+                    <option>Maranhão (MA)</option>
+                    <option>Mato Grosso (MT)</option>
+                    <option>Mato Grosso do Sul (MS)</option>
+                    <option>Minas Gerais (MG)</option>
+                    <option>Pará (PA)</option>
+                    <option>Paraíba (PB)</option>
+                    <option>Paraná (PR)</option>
+                    <option>Pernambuco (PE)</option>
+                    <option>Piauí (PI)</option>
+                    <option>Rio de Janeiro (RJ)</option>
+                    <option>Rio Grande do Norte (RN)</option>
+                    <option>Rio Grande do Sul (RS)</option>
+                    <option>Rondônia (RO)</option>
+                    <option>Roraima (RR)</option>
+                    <option>Santa Catarina (SC)</option>
+                    <option>São Paulo (SP)</option>
+                    <option>Sergipe (SE)</option>
+                    <option>Tocantins (TO)</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form.Row>
+              <Form.Row>
+                <Form.Group as={Col} md="3" controlId="formGridZip">
+                  <Form.Label>CEP</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="FALTOU INCLUIR NO CÓDIGO"
+                    // value={address.cep}
+                    // onChange={e =>
+                    //   setAddress({ ...address, state: e.target.value })
+                    // }
+                  />
+                </Form.Group>
+              </Form.Row>
+              <Button type="submit" className="user-button-save" variant="dark">
                 Salvar alterações
               </Button>
             </Form>
+            <hr></hr>
+            <div className="user-title-container">
+              <h4 className="">Excluir conta</h4>
+            </div>
             <Form.Label>
-              Ao deletar sua conta, você não será mais um membro da loja
+              Ao excluir sua conta, você não será mais um membro da loja
               Voluntei.
             </Form.Label>
             <Button
-              className="user-buttons shadow-sm"
+              className="user-button-delete"
               variant="dark"
               onClick={() => deleteProfile()}
             >
-              Excluir cadastro
+              Excluir conta
             </Button>
           </Card.Body>
         </Card>
