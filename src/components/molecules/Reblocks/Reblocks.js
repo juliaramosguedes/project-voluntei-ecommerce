e6ecd3a4d833a89e4d90e79a5753cc9cec9d9a55
luanperosa <luan.perosa@hotmail.com>
2019-12-14 17:37:46 -0300

    implement products page

--- src/components/molecules/Reblocks/Reblocks.js
+++ src/components/molecules/Reblocks/Reblocks.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { ReblocksPayment } from 'reblocks';
 
-export default function Reblocks({ history }) {
+export default function Reblocks({ totalPrice, successPayPal }) {
   const onSuccess = (data) => {
-    console.log(data.token, data.status)
-    history.push('/paymentSuccess');
+    successPayPal(data);
   };
 
   return (
     <>
       <ReblocksPayment
         accountId="xrb_3fb6yqcer7z9rmq3349mh31wr8jjj6y8nwm9kn4nrc7hu3pwmi84a596hj1c"
-        amount={1000000}
+        amount="1"
         onPaymentSuccess={onSuccess}
       />
     </>
