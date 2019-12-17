b449926e834322d5fa68327113f2180b9936a8c9
luanperosa <luan.perosa@hotmail.com>
2019-12-17 02:50:14 -0300

    refactor reblocks

--- src/components/molecules/Reblocks/Reblocks.js
+++ src/components/molecules/Reblocks/Reblocks.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReblocksPayment } from 'reblocks';
 
 export default function Reblocks({ totalPrice, successPayment }) {
+  const [price, setPrice] = useState(1)
+  
   const onSuccess = (data) => {
     successPayment(data);
   };
 
+  const changePrice = () => {
+    setPrice(totalPrice);
+  };
+  
   return (
     <>
       <ReblocksPayment
         accountId="xrb_3fb6yqcer7z9rmq3349mh31wr8jjj6y8nwm9kn4nrc7hu3pwmi84a596hj1c"
-        amount="1"
+        amount={price}
         onPaymentSuccess={onSuccess}
+        onClick={() => changePrice()}
       />
     </>
   );
